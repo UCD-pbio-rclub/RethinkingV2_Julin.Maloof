21807080cfbf466ef9c4a63f30cc70c8d777c730
Julin Maloof <jnmaloof@ucdavis.edu>
2019-10-03 17:17:25 -0700

    update book_code

--- book_code.R
+++ book_code.R
@@ -285,14 +285,11 @@ prior_h <- rnorm( 1e4 , sample_mu , sample_sigma )
 dens( prior_h )
 
 ## R code 4.16
-mu.list <- seq( from=140, to=160 , length.out=200 )
-sigma.list <- seq( from=4 , to=9 , length.out=200 )
+mu.list <- seq( from=150, to=160 , length.out=100 )
+sigma.list <- seq( from=7 , to=9 , length.out=100 )
 post <- expand.grid( mu=mu.list , sigma=sigma.list )
-post$LL <- sapply( 1:nrow(post) , function(i) sum( dnorm(
-                d2$height ,
-                mean=post$mu[i] ,
-                sd=post$sigma[i] ,
-                log=TRUE ) ) )
+post$LL <- sapply( 1:nrow(post) , function(i) sum(
+    dnorm( d2$height , post$mu[i] , post$sigma[i] , log=TRUE ) ) )
 post$prod <- post$LL + dnorm( post$mu , 178 , 20 , TRUE ) +
     dunif( post$sigma , 0 , 50 , TRUE )
 post$prob <- exp( post$prod - max(post$prod) )
@@ -512,7 +509,7 @@ mu_at_50 <- post$a + post$b * ( 50 - xbar )
 dens( mu_at_50 , col=rangi2 , lwd=2 , xlab="mu|weight=50" )
 
 ## R code 4.52
-HPDI( mu_at_50 , prob=0.89 )
+PI( mu_at_50 , prob=0.89 )
 
 ## R code 4.53
 mu <- link( m4.3 )
@@ -540,7 +537,7 @@ for ( i in 1:100 )
 ## R code 4.56
 # summarize the distribution of mu
 mu.mean <- apply( mu , 2 , mean )
-mu.HPDI <- apply( mu , 2 , HPDI , prob=0.89 )
+mu.PI <- apply( mu , 2 , PI , prob=0.89 )
 
 ## R code 4.57
 # plot raw data
@@ -550,8 +547,8 @@ plot( height ~ weight , data=d2 , col=col.alpha(rangi2,0.5) )
 # plot the MAP line, aka the mean mu for each weight
 lines( weight.seq , mu.mean )
 
-# plot a shaded region for 89% HPDI
-shade( mu.HPDI , weight.seq )
+# plot a shaded region for 89% PI
+shade( mu.PI , weight.seq )
 
 ## R code 4.58
 post <- extract.samples(m4.3)
@@ -702,6 +699,18 @@ plot( d2$year , d2$temp , col=col.alpha(rangi2,0.3) , pch=16 )
 shade( mu_PI , d2$year , col=col.alpha("black",0.5) )
 
 ## R code 4.79
+m4.7alt <- quap(
+    alist(
+        T ~ dnorm( mu , sigma ) ,
+        mu <- a + sapply( 1:1124 , function(i) sum( B[i,]*w ) ) ,
+        a ~ dnorm(6,10),
+        w ~ dnorm(0,1),
+        sigma ~ dexp(1)
+    ),
+    data=list( T=d2$temp , B=B ) ,
+    start=list( w=rep( 0 , ncol(B) ) ) )
+
+## R code 4.80
 plot( height ~ weight , data=Howell1 ,
     col=col.alpha(rangi2,0.4) )
 
@@ -2049,7 +2058,7 @@ precis( m8.5 , depth=2 )
 
 ## R code 9.11
 dat_slim <- list(
-    log_gpd_std = dd$log_gdp_std,
+    log_gdp_std = dd$log_gdp_std,
     rugged_std = dd$rugged_std,
     cid = as.integer( dd$cid )
 )
@@ -2093,6 +2102,9 @@ pairs( m9.1 )
 traceplot( m9.1 )
 
 ## R code 9.19
+trankplot( m9.1 , n_cols=2 )
+
+## R code 9.20
 y <- c(-1,1)
 set.seed(11)
 m9.2 <- ulam(
@@ -2104,10 +2116,10 @@ m9.2 <- ulam(
     ) ,
     data=list(y=y) , chains=2 )
 
-## R code 9.20
+## R code 9.21
 precis( m9.2 )
 
-## R code 9.21
+## R code 9.22
 set.seed(11)
 m9.3 <- ulam(
     alist(
@@ -2119,11 +2131,11 @@ m9.3 <- ulam(
     data=list(y=y) , chains=2 )
 precis( m9.3 )
 
-## R code 9.22
+## R code 9.23
 set.seed(41)
 y <- rnorm( 100 , mean=0 , sd=1 )
 
-## R code 9.23
+## R code 9.24
 m9.4 <- ulam(
     alist(
         y ~ dnorm( mu , sigma ) ,
@@ -2135,7 +2147,7 @@ m9.4 <- ulam(
     data=list(y=y) , chains=2 )
 precis( m9.4 )
 
-## R code 9.24
+## R code 9.25
 m9.5 <- ulam(
     alist(
         y ~ dnorm( mu , sigma ) ,
@@ -2147,7 +2159,7 @@ m9.5 <- ulam(
     data=list(y=y) , chains=2 )
 precis( m9.5 )
 
-## R code 9.25
+## R code 9.26
 mp <- map2stan(
     alist(
         a ~ dnorm(0,1),
@@ -2157,7 +2169,7 @@ mp <- map2stan(
     start=list(a=0,b=0),
     iter=1e4, warmup=100 , WAIC=FALSE )
 
-## R code 9.26
+## R code 9.27
 N <- 100                          # number of individuals
 height <- rnorm(N,10,2)           # sim total height of each
 leg_prop <- runif(N,0.4,0.5)      # leg as proportion of height
@@ -2168,7 +2180,7 @@ leg_right <- leg_prop*height +    # sim right leg as proportion + error
                                   # combine into data frame
 d <- data.frame(height,leg_left,leg_right)
 
-## R code 9.27
+## R code 9.28
 m5.8s <- map2stan(
     alist(
         height ~ dnorm( mu , sigma ) ,
@@ -2181,7 +2193,7 @@ m5.8s <- map2stan(
     data=d, chains=4,
     start=list(a=10,bl=0,br=0,sigma=1) )
 
-## R code 9.28
+## R code 9.29
 m5.8s2 <- map2stan(
     alist(
         height ~ dnorm( mu , sigma ) ,
@@ -2317,7 +2329,7 @@ mean( abs( p[,1] - p[,2] ) )
 dat_list <- list(
     pulled_left = d$pulled_left,
     actor = d$actor,
-    treatment = d$treatment )
+    treatment = as.integer(d$treatment) )
 
 # particles in 11-dimensional space
 m11.4 <- ulam(
@@ -3016,6 +3028,7 @@ pdat <- data.frame(A=kA,C=kC,I=kI)
 phi <- link( m12.6 , data=pdat )$phi
 
 ## R code 12.28
+post <- extract.samples( m12.6 )
 for ( s in 1:50 ) {
     pk <- pordlogit( 1:6 , phi[s,] , post$cutpoints[s,] )
     for ( i in 1:6 ) lines( kI , pk[,i] , col=col.alpha("black",0.1) )
@@ -4112,13 +4125,84 @@ for ( i in 1:nrow(d) )
     lines( c( dlist$M_obs[i] , M_est[i] ) , c( dlist$D_obs[i] , D_est[i] ) )
 
 ## R code 15.7
+N <- 500
+A <- rnorm(N)
+M <- rnorm(N,-A)
+D <- rnorm(N,A)
+A_obs <- rnorm(N,A)
+
+## R code 15.8
+N <- 100
+S <- rnorm( N )
+H <- rbinom( N , size=10 , inv_logit(S) )
+
+## R code 15.9
+D <- rbern( N ) # dogs completely random
+Hm <- H
+Hm[D==1] <- NA
+
+## R code 15.10
+D <- ifelse( S > 0 , 1 , 0 )
+Hm <- H
+Hm[D==1] <- NA
+
+## R code 15.11
+set.seed(501)
+N <- 1000
+X <- rnorm(N)
+S <- rnorm(N)
+H <- rbinom( N , size=10 , inv_logit( 2 + S - 2*X ) )
+D <- ifelse( X > 1 , 1 , 0 )
+Hm <- H
+Hm[D==1] <- NA
+
+## R code 15.12
+dat_list <- list(
+    H = H,
+    S = S )
+
+m15.3 <- ulam(
+    alist(
+        H ~ binomial( 10 , p ),
+        logit(p) <- a + bS*S,
+        a ~ normal( 0 , 1 ),
+        bS ~ normal( 0 , 0.5 )
+    ), data=dat_list , chains=4 )
+precis( m15.3 )
+
+## R code 15.13
+dat_list0 <- list(
+    H = H[D==0],
+    S = S[D==0] )
+
+m15.4 <- ulam(
+    alist(
+        H ~ binomial( 10 , p ),
+        logit(p) <- a + bS*S,
+        a ~ normal( 0 , 1 ),
+        bS ~ normal( 0 , 0.5 )
+    ), data=dat_list0 , chains=4 )
+precis( m15.4 )
+
+## R code 15.14
+D <- ifelse( abs(X) < 1 , 1 , 0 )
+
+## R code 15.15
+N <- 100
+S <- rnorm(N)
+H <- rbinom( N , size=10 , inv_logit(S) )
+D <- ifelse( H < 5 , 1 , 0 )
+Hm <- H
+Hm[D==1] <- NA
+
+## R code 15.16
 library(rethinking)
 data(milk)
 d <- milk
 d$neocortex.prop <- d$neocortex.perc / 100
 d$logmass <- log(d$mass)
 
-## R code 15.8
+## R code 15.17
 dat_list <- list(
     K = standardize( d$kcal.per.g ),
     B = standardize( d$neocortex.prop ),
@@ -4136,10 +4220,10 @@ m15.3 <- ulam(
         sigma ~ dexp( 1 )
     ) , data=dat_list , chains=4 , cores=4 )
 
-## R code 15.9
+## R code 15.18
 precis( m15.3 , depth=2 )
 
-## R code 15.10
+## R code 15.19
 obs_idx <- which( !is.na(d$neocortex.prop) )
 dat_list_obs <- list(
     K = dat_list$K[obs_idx],
@@ -4158,10 +4242,10 @@ m15.4 <- ulam(
     ) , data=dat_list_obs , chains=4 , cores=4 )
 precis( m15.4 )
 
-## R code 15.11
+## R code 15.20
 plot( coeftab(m15.3,m15.4) , pars=c("bB","bM") )
 
-## R code 15.12
+## R code 15.21
 post <- extract.samples( m15.3 )
 B_impute_mu <- apply( post$B_impute , 2 , mean )
 B_impute_ci <- apply( post$B_impute , 2 , PI )
@@ -4181,7 +4265,7 @@ Mi <- dat_list$M[miss_idx]
 points( Mi , B_impute_mu )
 for ( i in 1:12 ) lines( rep(Mi[i],2) , B_impute_ci[,i] )
 
-## R code 15.13
+## R code 15.22
 m15.5 <- ulam(
     alist(
        # K as function of B and M
@@ -4204,9 +4288,317 @@ m15.5 <- ulam(
     ) , data=dat_list , chains=4 , cores=4 )
 precis( m15.5 , depth=3 , pars=c("bM","bB","Rho_BM" ) )
 
-## R code 15.14
+## R code 15.23
+B_missidx <- which( is.na( dat_list$B ) )
+
+## R code 15.24
+data(Moralizing_gods)
+str(Moralizing_gods)
+
+## R code 15.25
+table( Moralizing_gods$moralizing_gods , useNA="always" )
+
+## R code 15.26
+symbol <- ifelse( Moralizing_gods$moralizing_gods==1 , 16 , 1 )
+symbol <- ifelse( is.na(Moralizing_gods$moralizing_gods) , 4 , symbol )
+color <- ifelse( is.na(Moralizing_gods$moralizing_gods) , "black" , rangi2 )
+plot( Moralizing_gods$year , Moralizing_gods$population , pch=symbol ,
+    col=color , xlab="Time (year)" , ylab="Population size" , lwd=1.5 )
+
+## R code 15.27
+dmg <- Moralizing_gods
+table( gods=dmg$moralizing_gods , literacy=dmg$writing , useNA="always" )
+
+## R code 15.28
+dmg <- Moralizing_gods
+haw <- which( dmg$polity=="Big Island Hawaii" )
+t( dmg[ haw , c("year","population","writing","moralizing_gods") ] )
+
+## R code 15.29
+set.seed(9)
+N_houses <- 100L
+alpha <- 5
+beta <- (-3)
+k <- 0.5
+r <- 0.2
+cat <- rbern( N_houses , k )
+notes <- rpois( N_houses , alpha + beta*cat )
+R_C <- rbern( N_houses , r )
+cat_obs <- cat
+cat_obs[R_C==1] <- (-9L)
+
+## R code 15.30
+dat <- list(
+    notes = notes,
+    cat = cat_obs,
+    RC = R_C,
+    N = as.integer(N_houses) )
+
+m15.6 <- ulam(
+    alist(
+        # singing bird model
+        ## cat known present/absent:
+        notes|RC==0 ~ poisson( lambda ),
+        log(lambda) <- a + b*cat,
+        ## cat NA:
+        notes|RC==1 ~ custom( log_sum_exp(
+                log(k) + poisson_lpmf( notes | exp(a + b) ),
+                log(1-k) + poisson_lpmf( notes | exp(a) )
+            ) ),
+
+        # priors
+        a ~ normal(0,1),
+        b ~ normal(0,0.5),
+
+        # sneaking cat model
+        cat|RC==0 ~ bernoulli(k),
+        k ~ beta(2,2)
+    ), data=dat , chains=4 , cores=4 )
+
+## R code 15.31
+m15.7 <- ulam(
+    alist(
+        # singing bird model
+        notes|RC==0 ~ poisson( lambda ),
+        notes|RC==1 ~ custom( log_sum_exp(
+                log(k) + poisson_lpmf( notes | exp(a + b) ),
+                log(1-k) + poisson_lpmf( notes | exp(a) )
+            ) ),
+        log(lambda) <- a + b*cat,
+        a ~ normal(0,1),
+        b ~ normal(0,0.5),
+
+        # sneaking cat model
+        cat|RC==0 ~ bernoulli(k),
+        k ~ beta(2,2),
+
+        # imputed values
+        gq> vector[N]:PrC1 <- exp(lpC1)/(exp(lpC1)+exp(lpC0)),
+        gq> vector[N]:lpC1 <- log(k) + poisson_lpmf( notes[i] | exp(a+b) ),
+        gq> vector[N]:lpC0 <- log(1-k) + poisson_lpmf( notes[i] | exp(a) )
+    ), data=dat , chains=4 , cores=4 )
+
+## R code 15.32
 set.seed(100)
 x <- c( rnorm(10) , NA )
 y <- c( rnorm(10,x) , 100 )
 d <- list(x=x,y=y)
+
+## R code 16.1
+library(rethinking)
+data(Howell1)
+d <- Howell1
+
+# scale observed variables
+d$w <- d$weight / mean(d$weight)
+d$h <- d$height / mean(d$height)
+
+## R code 16.2
+m16.1 <- ulam(
+    alist(
+        w ~ dlnorm( mu , sigma ),
+        exp(mu) <- 3.141593 * k * p^2 * h^3,
+        p ~ beta( 2 , 18 ),
+        k ~ exponential( 0.5 ),
+        sigma ~ exponential( 1 )
+    ), data=d , chains=4 , cores=4 )
+
+## R code 16.3
+h_seq <- seq( from=0 , to=max(d$h) , length.out=30 )
+w_sim <- sim( m16.1 , data=list(h=h_seq) )
+mu_mean <- apply( w_sim , 2 , mean )
+w_CI <- apply( w_sim , 2 , PI )
+plot( d$h , d$w , xlim=c(0,max(d$h)) , ylim=c(0,max(d$w)) , col=rangi2 ,
+    lwd=2 , xlab="height (scaled)" , ylab="weight (scaled)" )
+lines( h_seq , mu_mean )
+shade( w_CI , h_seq )
+
+## R code 16.4
+library(rethinking)
+data(Boxes)
+precis(Boxes)
+
+## R code 16.5
+table( Boxes$y ) / length( Boxes$y )
+
+## R code 16.6
+set.seed(7)
+N <- 30 # number of children
+
+# half are random
+# sample from 1,2,3 at random for each
+y1 <- sample( 1:3 , size=N/2 , replace=TRUE )
+
+# half follow majority
+y2 <- rep( 2 , N/2 )
+
+# combine and shuffle y1 and y2
+y <- sample( c(y1,y2) )
+
+# count the 2s
+sum(y==2)/N
+
+## R code 16.7
+data(Boxes_model)
+cat(Boxes_model)
+
+## R code 16.8
+# prep data
+dat_list <- list(
+    N = nrow(Boxes),
+    y = Boxes$y,
+    majority_first = Boxes$majority_first )
+
+# run the sampler
+m16.2 <- stan( model_code=Boxes_model , data=dat_list , chains=3 , cores=3 )
+
+# show marginal posterior for p
+p_labels <- c("1 Majority","2 Minority","3 Maverick","4 Random","5 Follow First")
+plot( precis(m16.2,2) , labels=p_labels )
+
+## R code 16.9
+library(rethinking)
+data(Panda_nuts)
+
+## R code 16.10
+N <- 1e4
+phi <- rlnorm( N , log(1) , 0.1 )
+k <- rlnorm( N , log(2), 0.25 )
+theta <- rlnorm( N , log(5) , 0.25 )
+
+# relative grow curve
+plot( NULL , xlim=c(0,1.5) , ylim=c(0,1) , xaxt="n" , xlab="age" ,
+    ylab="body mass" )
+at <- c(0,0.25,0.5,0.75,1,1.25,1.5)
+axis( 1 , at=at , labels=round(at*max(Panda_nuts$age)) )
+for ( i in 1:20 ) curve( (1-exp(-k[i]*x)) , add=TRUE , col=grau() , lwd=1.5 )
+
+# implied rate of nut opening curve
+plot( NULL , xlim=c(0,1.5) , ylim=c(0,1.2) , xaxt="n" , xlab="age" ,
+    ylab="nuts per second" )
+at <- c(0,0.25,0.5,0.75,1,1.25,1.5)
+axis( 1 , at=at , labels=round(at*max(Panda_nuts$age)) )
+for ( i in 1:20 ) curve( phi[i]*(1-exp(-k[i]*x))^theta[i] , add=TRUE ,
+    col=grau() , lwd=1.5 )
+
+## R code 16.11
+dat_list <- list(
+    n = as.integer( Panda_nuts$nuts_opened ),
+    age = Panda_nuts$age / max(Panda_nuts$age),
+    seconds = Panda_nuts$seconds )
+
+m16.4 <- ulam(
+    alist(
+        n ~ poisson( lambda ),
+        lambda <- seconds*phi*(1-exp(-k*age))^theta,
+        phi ~ lognormal( log(1) , 0.1 ),
+        k ~ lognormal( log(2) , 0.25 ),
+        theta ~ lognormal( log(5) , 0.25 )
+    ), data=dat_list , chains=4 )
+
+## R code 16.12
+post <- extract.samples(m16.4)
+plot( NULL , xlim=c(0,1) , ylim=c(0,1.5) , xlab="age" ,
+    ylab="nuts per second" , xaxt="n" )
+at <- c(0,0.25,0.5,0.75,1,1.25,1.5)
+axis( 1 , at=at , labels=round(at*max(Panda_nuts$age)) )
+
+# raw data
+pts <- dat_list$n / dat_list$seconds
+point_size <- normalize( dat_list$seconds )
+points( jitter(dat_list$age) , pts , col=rangi2 , lwd=2 , cex=point_size*3 )
+
+# 30 posterior curves
+for ( i in 1:30 ) with( post ,
+    curve( phi[i]*(1-exp(-k[i]*x))^theta[i] , add=TRUE , col=grau() ) )
+
+## R code 16.13
+library(rethinking)
+data(Lynx_Hare)
+plot( 1:21 , Lynx_Hare[,3] , ylim=c(0,90) , xlab="year" ,
+    ylab="thousands of pelts" , xaxt="n" , type="l" , lwd=1.5 )
+at <- c(1,11,21)
+axis( 1 , at=at , labels=Lynx_Hare$Year[at] )
+lines( 1:21 , Lynx_Hare[,2] , lwd=1.5 , col=rangi2 )
+points( 1:21 , Lynx_Hare[,3] , bg="black" , col="white" , pch=21 , cex=1.4 )
+points( 1:21 , Lynx_Hare[,2] , bg=rangi2 , col="white" , pch=21 , cex=1.4 )
+text( 17 , 80 , "Lepus" , pos=2 )
+text( 19 , 50 , "Lynx" , pos=2 , col=rangi2 )
+
+## R code 16.14
+sim_lynx_hare <- function( n_steps , init , theta , dt=0.002 ) {
+    L <- rep(NA,n_steps)
+    H <- rep(NA,n_steps)
+    L[1] <- init[1]
+    H[1] <- init[2]
+    for ( i in 2:n_steps ) {
+        H[i] <- H[i-1] + dt*H[i-1]*( theta[1] - theta[2]*L[i-1] )
+        L[i] <- L[i-1] + dt*L[i-1]*( theta[3]*H[i-1] - theta[4] )
+    }
+    return( cbind(L,H) )
+}
+
+## R code 16.15
+theta <- c( 0.5 , 0.05 , 0.025 , 0.5 )
+z <- sim_lynx_hare( 1e4 , as.numeric(Lynx_Hare[1,2:3]) , theta )
+
+plot( z[,2] , type="l" , ylim=c(0,max(z[,2])) , lwd=2 , xaxt="n" ,
+    ylab="number (thousands)" , xlab="" )
+lines( z[,1] , col=rangi2 , lwd=2 )
+mtext( "time" , 1 )
+
+## R code 16.16
+N <- 1e4
+Ht <- 1e4
+p <- rbeta(N,2,18)
+h <- rbinom( N , size=Ht , prob=p )
+h <- round( h/1000 , 2 )
+dens( h , xlab="thousand of pelts" , lwd=2 )
+
+## R code 16.17
+data(Lynx_Hare_model)
+cat(Lynx_Hare_model)
+
+## R code 16.18
+dat_list <- list(
+    N = nrow(Lynx_Hare),
+    pelts = Lynx_Hare[,2:3] )
+
+m16.5 <- stan( model_code=Lynx_Hare_model , data=dat_list , chains=3 , cores=3 ,
+    control=list( adapt_delta=0.95 ) )
+
+## R code 16.19
+post <- extract.samples(m16.5)
+pelts <- dat_list$pelts
+plot( 1:21 , pelts[,2] , pch=16 , ylim=c(0,120) , xlab="year" ,
+    ylab="thousands of pelts" , xaxt="n" )
+at <- c(1,11,21)
+axis( 1 , at=at , labels=Lynx_Hare$Year[at] )
+points( 1:21 , pelts[,1] , col=rangi2 , pch=16 )
+# 21 time series from posterior
+for ( s in 1:21 ) {
+    lines( 1:21 , post$pelts_pred[s,,2] , col=col.alpha("black",0.2) , lwd=2 )
+    lines( 1:21 , post$pelts_pred[s,,1] , col=col.alpha(rangi2,0.3) , lwd=2 )
+}
+# text labels
+text( 17 , 90 , "Lepus" , pos=2 )
+text( 19 , 50 , "Lynx" , pos=2 , col=rangi2 )
+
+## R code 16.20
+plot( NULL , pch=16 , xlim=c(1,21) , ylim=c(0,500) , xlab="year" ,
+    ylab="thousands of animals" , xaxt="n" )
+at <- c(1,11,21)
+axis( 1 , at=at , labels=Lynx_Hare$Year[at] )
+for ( s in 1:21 ) {
+    lines( 1:21 , post$pop[s,,2] , col=col.alpha("black",0.2) , lwd=2 )
+    lines( 1:21 , post$pop[s,,1] , col=col.alpha(rangi2,0.4) , lwd=2 )
+}
+
+## R code 16.21
+data(Lynx_Hare)
+dat_ar1 <- list(
+    L = Lynx_Hare$Lynx[2:21],
+    L_lag1 = Lynx_Hare$Lynx[1:20],
+    H = Lynx_Hare$Hare[2:21],
+    H_lag1 = Lynx_Hare$Hare[1:20] )
 
